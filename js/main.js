82749b6f35517ee50e4c8a3e2b653f2c71036ddb
dahee <opera9070@gmail.com>
2021-12-31 16:26:03 +0900

    feat: 계좌별로 섹션만듦

--- js/main.js
+++ js/main.js
@@ -5,5 +5,18 @@ fetch("./data/test.json")
 function start(accountArr) {
   const accounts = accountArr.accounts
   const accountsLength = accounts.length
+  console.log(accounts)
+
+  //첫번째 계좌인지 판별 후 섹션 클론
+  const main = document.querySelector('.main-contents-wrap')
+  const accountSection = document.querySelector('.view-page')
+  console.log(main, accountSection)
+  accounts.forEach((cur, index) => {
+    if (index !== 0) {
+      const cloneSection = accountSection.cloneNode(true)
+      cloneSection.className = `view-page swiper-slide accont-${index + 1}`
+      main.append(cloneSection)
+    }
+  })
 
 }
